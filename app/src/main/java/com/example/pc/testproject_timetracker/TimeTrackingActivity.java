01a302e4d905665e8f61eca16e3b6d9bd795a802
Saba-21 <stoningrolle@gmail.com>
2018-02-16 19:59:16 +0400

    Initial commit

---
+++ app/src/main/java/com/example/pc/testproject_timetracker/TimeTrackingActivity.java
@@ -0,0 +1,13 @@
+package com.example.pc.testproject_timetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimeTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_tracking);
+    }
+}
