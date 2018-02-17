c09a637bb42b5555375f7375a5244deb0bef9f7b
Saba-21 <stoningrolle@gmail.com>
2018-02-18 03:07:23 +0400

    timer setup, some logic

--- app/src/main/java/com/example/pc/testproject_timetracker/TimeTrackingActivity.java
+++ app/src/main/java/com/example/pc/testproject_timetracker/TimeTrackingActivity.java
@@ -1,6 +1,7 @@
 package com.example.pc.testproject_timetracker;
 
 import android.app.Dialog;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,14 +11,18 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TimeTrackingActivity extends AppCompatActivity{
 
     private Dialog firstDialog, secondDialog;
-    private EditText taskText, descriptionText, timer;
-    private String task, description, time;
+    private EditText taskView, descriptionView;
+    private TextView timeView;
+    private String task, description;
     private RVAdapter rvAdapter;
     private DBHelper dbHelper;
+    private int totalSeconds = 0;
+    private boolean run;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +37,6 @@ public class TimeTrackingActivity extends AppCompatActivity{
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         rvAdapter = new RVAdapter(dbHelper.getData());
         recyclerView.setAdapter(rvAdapter);
-
     }
 
     private void setupDialog(){
@@ -46,14 +50,12 @@ public class TimeTrackingActivity extends AppCompatActivity{
         Window dialogWindow = firstDialog.getWindow();
         if (dialogWindow != null) {
             params = dialogWindow.getAttributes();
-
             params.width = width;
             params.height = height;
             dialogWindow.setAttributes(params);
         }
         dialogWindow = secondDialog.getWindow();
         if (dialogWindow != null) {
-
             params = dialogWindow.getAttributes();
             params.width = width;
             params.height = height;
@@ -72,9 +74,9 @@ public class TimeTrackingActivity extends AppCompatActivity{
         secondDialog.setCancelable(false);
         secondDialog.setCanceledOnTouchOutside(false);
 
-        taskText = firstDialog.findViewById(R.id.task_text);
-        descriptionText = firstDialog.findViewById(R.id.description_text);
-        timer = secondDialog.findViewById(R.id.timer);
+        taskView = firstDialog.findViewById(R.id.task_text);
+        descriptionView = firstDialog.findViewById(R.id.description_text);
+        timeView = secondDialog.findViewById(R.id.timer);
     }
 
     public void newTaskButton(View view){
@@ -82,19 +84,43 @@ public class TimeTrackingActivity extends AppCompatActivity{
     }
 
     public void startButton(View view) {
-        firstDialog.dismiss();
-        task = taskText.getText().toString();
-        description = descriptionText.getText().toString();
-        secondDialog.show();
+        task = taskView.getText().toString();
+        taskView.getText().clear();
+        description = descriptionView.getText().toString();
+        descriptionView.getText().clear();
+        if (empty(task) && empty(description)) {
+            firstDialog.cancel();
+            secondDialog.show();
+            run = true;
+            timer();
+        }
     }
 
     public void finishButton(View view) {
-        time = timer.getText().toString();
-        if(empty(task) && empty(description) && empty(time)) {
-            dbHelper.addData(new DataModel(task, description, time));
-            rvAdapter.addItem(dbHelper.getData());
-        }
-        secondDialog.dismiss();
+        run = false;
+        String time = timeView.getText().toString();
+        totalSeconds = 0;
+        dbHelper.addData(new DataModel(task, description, time));
+        rvAdapter.addItem(dbHelper.getData());
+        secondDialog.cancel();
+    }
+
+    private void timer(){
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (run) {
+                    int hours = totalSeconds / 3600;
+                    int minutes = (totalSeconds % 3600) / 60;
+                    int seconds = totalSeconds % 60;
+                    String timeText = String.format("%d:%02d:%02d", hours, minutes, seconds);
+                    timeView.setText(timeText);
+                    totalSeconds++;
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        });
     }
 
     private boolean empty(String text) {
